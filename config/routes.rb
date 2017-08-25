a7e539b54753b2c0d4189f869f5f828c42d550eb
helloamos <salut.amos@gmail.com>
2017-08-25 17:05:30 +0200

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :suscribers
   #devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root to: "home#index"
+  root to: "pages#newsletter_suscribe"
 
   get "landing" => "pages#landing_page", as: :landing_page
   get "suscribe" => "pages#newsletter_suscribe", as: :suscribe
