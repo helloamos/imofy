782e59d94279ca2bde7ac93c018bc998c2d749d9
helloamos <salut.amos@gmail.com>
2017-09-11 14:35:29 +0000

    commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -87,12 +87,12 @@ Rails.application.configure do
 
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
-  config.action_mailer.default_url_options = { host: 'localhost:3000' }
+  config.action_mailer.default_url_options = { host: 'imofy.xyz' }
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     user_name:      'imofy.official@gmail.com',
     password:       'AMOSXZIBITDE88',
-    domain:         'localhost:3000',
+    domain:         'imofy.xyz',
     address:       'smtp.gmail.com',
     port:          '587',
     authentication: :plain,
